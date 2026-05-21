1defac4d0c441e1bd455b040d7473fa7162581d4
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-05-21 12:43:05 +0530

    Create python_if_else.py

---
+++ basics/python_if_else.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    n = int(input())
+    if n%2!=0:
+        print("Weird")
+    if n in range(2,6) and n%2==0:
+        print("Not Weird")
+    if n in range(6,21) and n%2==0:
+        print("Weird")
+    if n>20 and n%2==0:
+        print("Not Weird")
