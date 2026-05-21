30b4df4109f30c479aafe1674961fb18768c5449
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-05-21 12:55:52 +0530

    Create leap_year.py

---
+++ basics/leap_year.py
@@ -0,0 +1,17 @@
+year = int(input())
+def is_leap(year):
+    if year%400==0:
+        return True
+    elif year%100==0:
+        return False
+    elif year%4==0:
+        return True
+    else:
+        return False
+        
+    print(is_leap(year))
+    
+
+    
+
+
