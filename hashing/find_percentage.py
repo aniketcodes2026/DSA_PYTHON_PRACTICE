c76995a6aa10ab2910cb45f228afb3835e3eabae
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-05-21 13:31:10 +0530

    Create find_percentage.py

---
+++ hashing/find_percentage.py
@@ -0,0 +1,14 @@
+# Problem: Finding the Percentage
+# Platform: HackerRank
+# Concept: Dictionaries / Hash Maps
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    scores=student_marks[query_name]
+    avg=sum(scores)/len(scores)
+    print(f"{avg:.2f}")
