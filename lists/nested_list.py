fb059d764fe3626ed0cee4c4d2b0a0ac663bdf37
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-05-21 13:28:15 +0530

    Create nested_list.py

---
+++ lists/nested_list.py
@@ -0,0 +1,21 @@
+# Problem: Nested Lists
+# Platform: HackerRank
+# Concept: Nested Lists / Sorting
+if __name__ == '__main__':
+    students=[]
+    for _ in range(int(input())):
+        name = str(input())
+        score = float(input())
+        students.append([name,score])
+    scores=[]
+    for student in students:
+        scores.append(student[1])
+    scores=sorted(set(scores))
+    second_lowest=scores[1]
+    result=[]
+    for student in students:
+        if student[1]==second_lowest:
+            result.append(student[0])
+    result.sort()
+    for name in result:
+        print(name)
