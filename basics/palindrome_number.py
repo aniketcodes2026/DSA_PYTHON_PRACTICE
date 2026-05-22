e3d0347125e760576a11e901f113fa85e8fb2876
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-05-22 15:19:01 +0530

    Create palindrome_number.py

---
+++ basics/palindrome_number.py
@@ -0,0 +1,15 @@
+# Problem: Palindrome Number
+# Platform: LeetCode
+# Concept: Number Manipulation
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0:
+            return False
+        div = 1
+        while x>=10*div:
+            div*=10
+        while div>1:
+            left=x/div
+            right=x%10
+            if left != right:
+                return False
