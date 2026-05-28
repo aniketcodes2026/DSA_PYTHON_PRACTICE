5cffccdfce4552bf39a2574ad241c44ac89a9071
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-05-28 21:37:17 +0530

    Create whats_your_name.py

---
+++ strings/whats_your_name.py
@@ -0,0 +1,7 @@
+# Problem: What's Your Name?
+# Platform: HackerRank
+# Concept: Strings / Functions
+
+def print_full_name(first, last):
+    
+    print(f"Hello {first} {last}! You just delved into python.")
