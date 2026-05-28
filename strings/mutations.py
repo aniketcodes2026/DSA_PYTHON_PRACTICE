881385399acfc2125c37901741c35120503a85c6
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-05-28 21:50:10 +0530

    Create mutations.py

---
+++ strings/mutations.py
@@ -0,0 +1,7 @@
+# Problem: Mutations
+# Platform: HackerRank
+# Concept: Strings
+
+
+def mutate_string(string, position, character):
+    return string[:position] + character + string[position + 1:]
