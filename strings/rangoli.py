9d96c0d605768b997452a8abe37f98690999b960
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-05-30 12:17:35 +0530

    Create rangoli.py

---
+++ strings/rangoli.py
@@ -0,0 +1,18 @@
+# Problem: Alphabet Rangoli
+# Platform: HackerRank
+# Concept: Strings / Pattern Printing
+
+
+def print_rangoli(size):
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+    width = 4 * size - 3
+
+    for i in range(size - 1, -1, -1):
+        s = "-".join(alpha[i:size])
+        row = s[::-1] + s[1:]
+        print(row.center(width, "-"))
+
+    for i in range(1, size):
+        s = "-".join(alpha[i:size])
+        row = s[::-1] + s[1:]
+        print(row.center(width, "-"))
