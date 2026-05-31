c6f01f912843e7e4a7774e4f482cec02ff0cb6b9
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-05-31 12:58:57 +0530

    Create merge_tools.py

---
+++ strings/merge_tools.py
@@ -0,0 +1,18 @@
+problem = the merge tools
+platform = hackerrank
+cconcept = strings/sets
+
+
+def merge_the_tools(string, k):
+    for i in range(0, len(string), k):
+        substring = string[i:i+k]
+
+        seen = set()
+        result = ""
+
+        for ch in substring:
+            if ch not in seen:
+                seen.add(ch)
+                result += ch
+
+        print(result)
