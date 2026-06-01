b0ca0130735e7e370db7706c3f415221cc7cf5b6
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-06-01 22:12:36 +0530

    Create itertool_combination.py

---
+++ itertools/itertool_combination.py
@@ -0,0 +1,12 @@
+# Problem: itertools.combinations()
+# Platform: HackerRank
+# Concept: Itertools / Combinations
+
+
+from itertools import combinations
+
+s, k = input().split()
+
+for i in range(1, int(k) + 1):
+    for c in combinations(sorted(s), i):
+        print(''.join(c))
