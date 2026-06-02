32c9c9d2bc909e4f95f15af83c41b9a4d0cace2f
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-06-02 22:01:03 +0530

    Create defaultdict_tutorial.py

---
+++ collections/defaultdict_tutorial.py
@@ -0,0 +1,21 @@
+# Problem: DefaultDict Tutorial
+# Platform: HackerRank
+# Concept: Collections / defaultdict
+
+from collections import defaultdict
+
+n, m = map(int, input().split())
+
+d = defaultdict(list)
+
+for i in range(1, n + 1):
+    word = input()
+    d[word].append(i)
+
+for _ in range(m):
+    query = input()
+
+    if query in d:
+        print(*d[query])
+    else:
+        print(-1)
