d7678f1d995e26e212ca734b88552f637b5a7d62
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-06-04 19:45:29 +0530

    Create intro_sets.py

---
+++ SETS/intro_sets.py
@@ -0,0 +1,7 @@
+# Problem: Introduction to Sets
+# Platform: HackerRank
+# Concept: Sets
+
+def average(array):
+    distinct_heights = set(array)
+    return sum(distinct_heights) / len(distinct_heights)
