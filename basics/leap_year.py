0efd9171551305301a6bd5270aaecda98f91100e
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-06-07 14:55:01 +0530

    Update leap_year.py

--- basics/leap_year.py
+++ basics/leap_year.py
@@ -1,10 +1,10 @@
 year = int(input())
 def is_leap(year):
-    if year%400==0:
+    if year%400==0:   #remainder when divided by 400 = 0
         return True
-    elif year%100==0:
+    elif year%100==0: #remainder when divided by 100 = 0
         return False
-    elif year%4==0:
+    elif year%4==0:   #remainder when divided by 4 = 0
         return True
     else:
         return False
