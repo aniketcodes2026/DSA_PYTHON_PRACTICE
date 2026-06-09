fed1fc8fb922f5b0fc8ea89ca72e178aa6938176
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-06-09 07:58:17 +0530

    Update triangle_quest_2.py

--- basics/triangle_quest_2.py
+++ basics/triangle_quest_2.py
@@ -1,6 +1,7 @@
 # Problem: Triangle Quest 2
 # Platform: HackerRank
 # Concept: Pattern Printing
+# Condition: should be in less than 3 lines of code
 
 for i in range(1, int(input()) + 1):
     print(((10**i - 1) // 9) ** 2)
