0ac7afa630349b4b812951b730d63e911e2fc0c2
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-06-11 22:06:15 +0530

    Update capitalize.py

--- strings/capitalize.py
+++ strings/capitalize.py
@@ -2,5 +2,5 @@
 # Platform: HackerRank
 # Concept: Strings
 
-def solve(s):
-    return ' '.join(word.capitalize() for word in s.split(' '))
+def capital(s):
+    return ' '.join(word.capitalize() for word in s.split(' ')) #makes the first letter of the word capital
