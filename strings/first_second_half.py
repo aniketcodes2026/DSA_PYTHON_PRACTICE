7a2f3c784df6cf056c9a49fe150b990c5c74c861
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-06-12 20:31:35 +0530

    Update first_second_half.py

--- strings/first_second_half.py
+++ strings/first_second_half.py
@@ -5,5 +5,5 @@ s = input("Enter a string: ")
 
 mid = len(s) // 2
 
-print("First Half:", s[:mid])
-print("Second Half:", s[mid:])
+print("First Half:", s[:mid]) #print characters from the start to the middle index (exclusive)
+print("Second Half:", s[mid:]) #print characters from the middle index to the end of the string
