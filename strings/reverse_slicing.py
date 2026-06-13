66f1c6e359171ca659df4695f3a2edc70a9193e3
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-06-13 19:18:35 +0530

    Update reverse_slicing.py

--- strings/reverse_slicing.py
+++ strings/reverse_slicing.py
@@ -1,6 +1,7 @@
 # Problem: Reverse String Using Slicing
 # Concept: String Slicing
+# Platform: hackerRank
 
 s = input("Enter a string: ")
 
-print(s[::-1])
+print(s[::-1]) #print the string in reverse order using slicing
