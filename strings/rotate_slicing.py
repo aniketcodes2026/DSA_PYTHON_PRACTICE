99013dfaab128b016dbfecd26a5da54a0292aaa6
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-06-13 19:19:17 +0530

    Update rotate_slicing.py

--- strings/rotate_slicing.py
+++ strings/rotate_slicing.py
@@ -1,8 +1,9 @@
 # Problem: Rotate String
 # Concept: String Slicing
+# Platform: hackerRank
 
 s = input("Enter a string: ")
 
 rotated = s[1:] + s[0]
 
-print(rotated)
+print(rotated) # print the rotated string by slicing the first character and concatenating it to the end of the string
