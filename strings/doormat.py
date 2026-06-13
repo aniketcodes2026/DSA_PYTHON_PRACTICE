121a10985391b0d3d721e9e31ae0b421dffff390
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-06-13 19:16:57 +0530

    Update doormat.py

--- strings/doormat.py
+++ strings/doormat.py
@@ -4,13 +4,11 @@
 
 
 n, m = map(int, input().split())
-
 for i in range(n // 2):
     pattern = ".|." * (2 * i + 1)
-    print(pattern.center(m, "-"))
-
-print("WELCOME".center(m, "-"))
+    print(pattern.center(m, "-")) #print pattern in the middle
+print("WELCOME".center(m, "-")) #print welcome in the middle
 
 for i in range(n // 2 - 1, -1, -1):
     pattern = ".|." * (2 * i + 1)
-    print(pattern.center(m, "-"))
+    print(pattern.center(m, "-")) #print pattern in the middle
