0a8032b83ee9a8b1a8e7f889391fd3213be329a6
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-06-14 20:31:20 +0530

    Update text_wrap.py

--- strings/text_wrap.py
+++ strings/text_wrap.py
@@ -5,4 +5,4 @@
 import textwrap
 
 def wrap(string, max_width):
-    return textwrap.fill(string, max_width)
+    return textwrap.fill(string, max_width) # textwrap.fill() is used to wrap the input string into lines of specified width. It takes the input string and the maximum width as arguments and returns a new string with the wrapped text.
