1a52e2914781b309476ab8edf0b53406f597e5b4
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-06-14 20:37:27 +0530

    Update whats_your_name.py

--- strings/whats_your_name.py
+++ strings/whats_your_name.py
@@ -4,4 +4,4 @@
 
 def print_full_name(first, last):
     
-    print(f"Hello {first} {last}! You just delved into python.")
+    print(f"Hello {first} {last}! You just delved into python.") # The function print_full_name takes two parameters, first and last, which represent the first and last names of a person. It then prints a greeting message that includes the full name using an f-string for formatting.
