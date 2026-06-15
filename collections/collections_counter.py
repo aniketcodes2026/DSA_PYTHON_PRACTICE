12804dcbe0515f1751769f1ee1ebdf3404186151
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-06-15 10:48:06 +0530

    Update collections_counter.py

--- collections/collections_counter.py
+++ collections/collections_counter.py
@@ -5,17 +5,17 @@
 from collections import Counter
 
 n = int(input())
-shoes = list(map(int, input().split()))
+shoes = list(map(int, input().split())) # read the number of shoes and the sizes of the shoes available in the store, and store them in a list called shoes
 
 counter = Counter(shoes)
 
 money = 0
 
 for _ in range(int(input())):
-    size, price = map(int, input().split())
+    size, price = map(int, input().split()) # read the size and price of the shoe being sold
 
     if counter[size] > 0:
         money += price
         counter[size] -= 1
 
-print(money)
+print(money) # print the total money earned from selling the shoes
