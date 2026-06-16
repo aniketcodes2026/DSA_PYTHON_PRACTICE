de472c22485ced28c39652e1d235c4d04d229b9f
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-06-16 19:55:17 +0530

    Update polar_coordinates.py

--- MATHS/polar_coordinates.py
+++ MATHS/polar_coordinates.py
@@ -6,5 +6,5 @@ import cmath
 
 z = complex(input())
 
-print(abs(z))
-print(cmath.phase(z))
+print(abs(z)) #prints the magnitude of the complex number z, which represents the polar coordinates of the complex number in the form (r, θ) where r is the magnitude and θ is the phase angle in radians.
+print(cmath.phase(z)) #prints the magnitude and phase of the complex number z, which represents the polar coordinates of the complex number in the form (r, θ) where r is the magnitude and θ is the phase angle in radians.
