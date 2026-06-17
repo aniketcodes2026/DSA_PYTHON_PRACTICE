446b240b9b195a7f46c24e929e7f91d272a1435a
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-06-17 15:59:35 +0530

    Update dot_and_cross.py

--- numpy/dot_and_cross.py
+++ numpy/dot_and_cross.py
@@ -6,7 +6,7 @@ import numpy
 
 n = int(input())
 
-a = numpy.array([input().split() for _ in range(n)], int)
-b = numpy.array([input().split() for _ in range(n)], int)
+a = numpy.array([input().split() for _ in range(n)], int) # List comprehension to read n lines of input and create a 2D array (matrix) of integers
+b = numpy.array([input().split() for _ in range(n)], int) # The above line does the same for the second matrix
 
-print(numpy.dot(a, b))
+print(numpy.dot(a, b)) # The numpy.dot() function computes the dot product of two arrays. For 2-D arrays, it is equivalent to matrix multiplication.
