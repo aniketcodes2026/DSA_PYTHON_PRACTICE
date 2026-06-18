91b61d5dc63685f362a1c6b510080b98005f74e2
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-06-18 19:27:14 +0530

    Update factorial.py

--- recursion/factorial.py
+++ recursion/factorial.py
@@ -5,8 +5,8 @@ def factorial(n):
     if n == 0 or n == 1:
         return 1
     
-    return n * factorial(n - 1)
+    return n * factorial(n - 1) # n! = n * (n-1)!
 
 
 num = int(input("Enter a number: "))
-print("Factorial =", factorial(num))
+print("Factorial =", factorial(num)) # prints the factorial of the input number
