7698e068422170de2f219b5ef4a78f93e9114141
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-06-19 21:00:19 +0530

    Update power_of_number.py

--- recursion/power_of_number.py
+++ recursion/power_of_number.py
@@ -1,10 +1,11 @@
 # Problem: Power of a Number
 # Concept: Recursion
+# Platform: HackerRank
 
 def power(a, b):
     if b == 0:
         return 1
 
-    return a * power(a, b - 1)
+    return a * power(a, b - 1) # Recursion: a^b = a * a^(b-1)
 
 print(power(2, 5))
