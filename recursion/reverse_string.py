c31a227c4bd89fa9404f25db792b5f0adeab40a4
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-06-19 21:01:57 +0530

    Update reverse_string.py

--- recursion/reverse_string.py
+++ recursion/reverse_string.py
@@ -1,10 +1,11 @@
 # Problem: Reverse String Using Recursion
 # Concept: Recursion / Strings
+# Platform: HackerRank
 
 def reverse_string(s):
     if len(s) == 0:
         return s
 
-    return reverse_string(s[1:]) + s[0]
+    return reverse_string(s[1:]) + s[0] # Recursion: reverse(s) = reverse(s[1:]) + s[0]
 
 print(reverse_string("hello"))
