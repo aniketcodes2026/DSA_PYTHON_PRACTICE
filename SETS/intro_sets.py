800b10a569cdb3dec4690526f14653ef21f09286
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-06-20 21:53:36 +0530

    Update intro_sets.py

--- SETS/intro_sets.py
+++ SETS/intro_sets.py
@@ -3,5 +3,5 @@
 # Concept: Sets
 
 def average(array):
-    distinct_heights = set(array)
-    return sum(distinct_heights) / len(distinct_heights)
+    distinct_heights = set(array) # Create a set to store distinct heights
+    return sum(distinct_heights) / len(distinct_heights) # Calculate the average of distinct heights
