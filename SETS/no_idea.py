abfe8bb11ae86582c188bc4fd440554ec0b52044
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-06-20 21:54:58 +0530

    Update no_idea.py

--- SETS/no_idea.py
+++ SETS/no_idea.py
@@ -4,10 +4,10 @@
 
 n, m = map(int, input().split())
 
-arr = list(map(int, input().split()))
+arr = list(map(int, input().split())) # Read the array of integers
 
-A = set(map(int, input().split()))
-B = set(map(int, input().split()))
+A = set(map(int, input().split())) # Read the set A
+B = set(map(int, input().split())) # Read the set B
 
 happiness = 0
 
