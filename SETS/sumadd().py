fdb740e19cfede5bf1c367b435540b497bc5867f
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-06-21 10:55:35 +0530

    Update sumadd().py

--- SETS/sumadd().py
+++ SETS/sumadd().py
@@ -4,7 +4,7 @@
 
 countries = set()
 
-for _ in range(int(input())):
-    countries.add(input())
+for _ in range(int(input())): 
+    countries.add(input()) # add() method adds an element to the set if it is not already present. If the element is already present, the set remains unchanged.
 
 print(len(countries))
