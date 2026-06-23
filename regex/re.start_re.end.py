37d90f1e13e620ba4fddf4038c7cf2ed85c5a619
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-06-23 19:21:23 +0530

    Update re.start_re.end.py

--- regex/re.start_re.end.py
+++ regex/re.start_re.end.py
@@ -8,12 +8,12 @@ import re
 S = input()
 k = input()
 
-matches = list(re.finditer(f'(?={k})', S))
+matches = list(re.finditer(f'(?={k})', S)) # Find all overlapping matches of the substring k in the string S
 
 if matches:
     for match in matches:
-        start = match.start()
-        end = start + len(k) - 1
+        start = match.start() # Get the starting index of the match
+        end = start + len(k) - 1 # Get the ending index of the match
         print((start, end))
 else:
     print((-1, -1))
