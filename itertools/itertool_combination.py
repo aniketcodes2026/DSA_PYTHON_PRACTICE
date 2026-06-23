3ec48c0428ae05ada6bfee4c228542900b8521ca
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-06-23 22:18:47 +0530

    Update itertool_combination.py

--- itertools/itertool_combination.py
+++ itertools/itertool_combination.py
@@ -5,8 +5,8 @@
 
 from itertools import combinations
 
-s, k = input().split()
+s, k = input().split() # Read the string and the maximum combination size
 
-for i in range(1, int(k) + 1):
-    for c in combinations(sorted(s), i):
+for i in range(1, int(k) + 1): # Loop through all combination sizes from 1 to k
+    for c in combinations(sorted(s), i): # Generate all combinations of the sorted string s of length i
         print(''.join(c)) #prints all the combinations of the characters in the string s with length from 1 to k, sorted in lexicographic order
