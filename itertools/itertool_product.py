be84ebe1193a68224890019d14d1ceace014f2a2
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-06-23 22:16:14 +0530

    Update itertool_product.py

--- itertools/itertool_product.py
+++ itertools/itertool_product.py
@@ -5,7 +5,7 @@
 
 from itertools import product
 
-A = list(map(int, input().split()))
-B = list(map(int, input().split()))
+A = list(map(int, input().split())) # Read the first list of integers
+B = list(map(int, input().split())) # Read the second list of integers
 
-print(*product(A, B))
+print(*product(A, B)) # Print the Cartesian product of lists A and B
