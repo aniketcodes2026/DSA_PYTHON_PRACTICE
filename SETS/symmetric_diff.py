4f819944705f257c513e31514e2b4e59192e1eef
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-06-24 19:15:52 +0530

    Update symmetric_diff.py

--- SETS/symmetric_diff.py
+++ SETS/symmetric_diff.py
@@ -10,5 +10,5 @@ B = set(map(int, input().split())) # Similarly, this line creates set B from the
 
 result = A.symmetric_difference(B) # The symmetric_difference() method returns a set that contains all items from both sets, except items that are present in both sets. In other words, it returns the elements that are in either set A or set B but not in both.
 
-for num in sorted(result):
+for num in sorted(result): # Sort the result and print each element
     print(num)
