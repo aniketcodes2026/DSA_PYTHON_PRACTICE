61057b2cbc3f20c25990152881fef905f8bbf64a
ANIKET RAJ <aniketvmc2008@gmail.com>
2026-07-04 15:11:51 +0530

    Add example usage for wrap function in text_wrap.py
    Added an example usage for the wrap function.

--- strings/text_wrap.py
+++ strings/text_wrap.py
@@ -6,3 +6,6 @@ import textwrap
 
 def wrap(string, max_width):
     return textwrap.fill(string, max_width) # textwrap.fill() is used to wrap the input string into lines of specified width. It takes the input string and the maximum width as arguments and returns a new string with the wrapped text.
+
+#EXAMPLE:
+print(wrap(aniketrajislearningpython))
