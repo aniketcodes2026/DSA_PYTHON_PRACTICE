4a512f1b5ef39a748177f96a959903971dc76da0
aniketcodes@123 <aniketvmc2008@gmail.com>
2026-07-05 19:16:20 +0530

    Update find_a_string.py

--- strings/find_a_string.py
+++ strings/find_a_string.py
@@ -7,7 +7,7 @@ def count_substring(string, sub_string):
     count = 0
 
     for i in range(len(string)):
-        if string[i:].startswith(sub_string):
+        if string[i:].startswith(sub_string): #startswith() method returns True if the string starts with the specified prefix, otherwise False
             count += 1
 
     return count
