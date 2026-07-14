b77a2dad173873ff7b34319e39f72c87b181cfa5
ANIKET RAJ <aniketvmc2008@gmail.com>
2026-07-14 19:44:46 +0530

    Fix indentation in arithmetic_operator.py

--- basics/arithmetic_operator.py
+++ basics/arithmetic_operator.py
@@ -5,8 +5,9 @@ Platform: HackerRank
 a = int(input(Enter a number:))
 b = int(input(Enter a number:))
 
-    sum=a+b  #adds two nums
-    diff=a-b  #subtract two nums
-    prod=a*b  #multiply two nums
-    print(sum, diff, prod, sep='\n')  #prints the results on new lines
+sum=a+b  #adds two nums
+diff=a-b  #subtract two nums
+prod=a*b  #multiply two nums
+    
+print(sum, diff, prod, sep='\n')  #prints the results on new lines
     
