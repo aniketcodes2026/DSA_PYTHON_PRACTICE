09b3e09c38ed3f8d84ac19b44286f53a5afa0f10
ANIKET RAJ <aniketvmc2008@gmail.com>
2026-07-14 19:43:36 +0530

    Update two_sum.py

--- arrays/two_sum.py
+++ arrays/two_sum.py
@@ -3,6 +3,7 @@
 # Concept: Arrays / Hash Maps
 
 class Solution(object):
+    
     def twoSum(self, nums, target): 
         for i in range(len(nums)): #\
             for j in range(i+1,len(nums)):
