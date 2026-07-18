c125b24b119f68dd116b012e4a37afeee62bf5ad
ANIKET RAJ <aniketvmc2008@gmail.com>
2026-07-18 19:54:51 +0530

    Update triangle_quest.py

--- basics/triangle_quest.py
+++ basics/triangle_quest.py
@@ -1,7 +1,7 @@
 # Problem: Triangle Quest
 # Platform: HackerRank
+# Condition: should be done in not more than 2 lines of code
 # Concept: Loops / Pattern Printing
-# Condition:  should be done in not more than 2 lines of code
 
 for i in range(1,int(input())+1):
     print(i*((10**i-1)//9))
