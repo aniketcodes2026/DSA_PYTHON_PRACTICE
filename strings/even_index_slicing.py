83e532dd6ed469b92ee30f54192304ff5cc42986
ANIKET RAJ <aniketvmc2008@gmail.com>
2026-07-20 20:57:31 +0530

    Update even_index_slicing.py

--- strings/even_index_slicing.py
+++ strings/even_index_slicing.py
@@ -1,5 +1,6 @@
 # Problem: Extract Even Index Characters
 # Concept: String Slicing
+# Platform: Hackerrank
 
 s = input("Enter a string: ")
 print(s[::2]) #print characters at even indices (0, 2, 4, ...)
