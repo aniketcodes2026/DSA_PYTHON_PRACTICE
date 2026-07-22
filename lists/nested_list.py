dd1fb57e4c29041424cd67925f9f27c7f5f15fea
ANIKET RAJ <aniketvmc2008@gmail.com>
2026-07-22 21:53:56 +0530

    Update nested_list.py

--- lists/nested_list.py
+++ lists/nested_list.py
@@ -8,12 +8,15 @@ for _ in range(int(input())):
     name = str(input())
     score = float(input())
     students.append([name,score])
+
 scores=[]
+
 for student in students:
     scores.append(student[1])
 scores=sorted(set(scores))
 second_lowest=scores[1]
 result=[]
+
 for student in students:
     if student[1]==second_lowest:
         result.append(student[0])
