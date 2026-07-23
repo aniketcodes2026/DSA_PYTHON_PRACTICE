9685e591d3bb3a21d07f589c2dd3966cb7470bbb
ANIKET RAJ <aniketvmc2008@gmail.com>
2026-07-23 18:11:35 +0530

    Update arithmetic_operator.py

--- basics/arithmetic_operator.py
+++ basics/arithmetic_operator.py
@@ -5,9 +5,9 @@
 a = int(input(Enter a number:))
 b = int(input(Enter a number:))
 
-sum=a+b  #adds two nums
-diff=a-b  #subtract two nums
-prod=a*b  #multiply two nums
+sum = a+b  #adds two nums
+diff = a-b  #subtract two nums
+prod = a*b  #multiply two nums
     
 print(sum, diff, prod, sep='\n')  #prints the results on new lines
     
