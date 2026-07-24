f317945a799071c8a2a585bd0824c4bbd92ad0c8
ANIKET RAJ <aniketvmc2008@gmail.com>
2026-07-24 19:43:50 +0530

    Update linear_alg.py

--- numpy/linear_alg.py
+++ numpy/linear_alg.py
@@ -6,12 +6,12 @@ import numpy
 
 n=int(input())
 
-array=numpy.array([input().split() for _ in range(n)],float) # List comprehension to read n lines of input and create a 2D array (matrix) of floats
-print(round(numpy.linalg.det(array),2)) #The linalg.det tool computes the determinant of an array. #round off to 2 decimal places
+array=numpy.array([input().split() for _ in range(n)],float)  # List comprehension to read n lines of input and create a 2D array (matrix) of floats
+print(round(numpy.linalg.det(array),2))  #The linalg.det tool computes the determinant of an array. #round off to 2 decimal places
  
 vals,vecs = numpy.linalg.eig(array)  #The linalg.eig tool computes the eigenvalues and right eigenvectors of a square array.
 
-print(vals) #The eigenvalues are returned in a 1D array, and the corresponding right eigenvectors are returned in a 2D array, where the i-th column is the eigenvector corresponding to the i-th eigenvalue.
-print(vecs) #The eigenvalues are returned in a 1D array, and the corresponding right eigenvectors are returned in a 2D array, where the i-th column is the eigenvector corresponding to the i-th eigenvalue.
+print(vals)  #The eigenvalues are returned in a 1D array, and the corresponding right eigenvectors are returned in a 2D array, where the i-th column is the eigenvector corresponding to the i-th eigenvalue.
+print(vecs)  #The eigenvalues are returned in a 1D array, and the corresponding right eigenvectors are returned in a 2D array, where the i-th column is the eigenvector corresponding to the i-th eigenvalue.
 
 print(numpy.linalg.inv(array))  #The linalg.inv tool computes the (multiplicative) inverse of a matrix.
